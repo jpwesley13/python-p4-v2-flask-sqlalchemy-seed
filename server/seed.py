79959558271d26e58e5cac9b7bce5f52bedd4af3
Jared Patterson <jpwesley34@gmail.com>
2024-06-19 11:55:27 -0700

    follows along.

--- server/seed.py
+++ server/seed.py
@@ -6,13 +6,16 @@ from models import db, Pet
 
 with app.app_context():
 
+    Pet.query.delete()
+
     # Create an empty list
     pets = []
 
     # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
+    pets.append(Pet(name = "Ali", species = "Dog"))
+    pets.append(Pet(name = "Cheshire", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Emeris", species = "Wolf"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
